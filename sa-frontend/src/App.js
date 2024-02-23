cf9d614976e5c9a65147c99c071ebb1baf65d099
Chetan Pathade <chetanpathade1997@gmail.com>
2024-02-22 18:04:12 -0800

    add file

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://34.86.26.144:80/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
